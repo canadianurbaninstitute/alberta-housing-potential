43420e11e177e4bd7a8b45fed66e7815a7e8805a
iw-00 <133152066+iw-00@users.noreply.github.com>
2026-08-28 14:34:06 -0400

    population tables

---
+++ README.md
@@ -0,0 +1,3 @@
+Statistics Canada. Table 17-10-0155-01 Population estimates, July 1, by census subdivision, 2021 boundaries
+
+Statistics Canada. Table 17-10-0162-01 Projected population for census divisions and census subdivisions, 2021 boundaries, by projection scenario, age and gender, as of July 1
---
+++ scripts/00_longlist.R
@@ -0,0 +1,72 @@
+# Data prep for municipality longlist
+
+library(msmdata)
+library(tidyverse)
+library(leaflet)
+library(sf)
+
+options(scipen = 999)
+
+# Data ----------------
+pop_est <- read_csv("input/population_estimates.csv")
+pop_proj <- read_csv("input/population_projections.csv")
+
+msn_network <- read_blob_data("Data/spatialfiles/msn_mainstreets.parquet", type = "geoparquet") |>
+  mutate(id = as.character(id))
+
+msn_alberta <- msn_network |>
+  filter(PRUID == 48)
+
+# Join ----------------
+
+pop_est <- pop_est |>
+  mutate(CSDNAME = str_extract(Geography, "^[^(]+") |> str_trim())
+
+pop_proj <- pop_proj |>
+  mutate(CSDNAME = str_extract(Geography, "^[^,]+") |> str_trim())
+
+pop_table <- pop_est |>
+  select(CSDNAME, `2021`, `2025`) |>
+  inner_join(
+    pop_proj |>
+      select(CSDNAME, `Projection scenario HG: high-growth 3`),
+    by = "CSDNAME"
+  ) |>
+  rename(
+    population_2021 = `2021`,
+    population_2025 = `2025`,
+    population_2050 = `Projection scenario HG: high-growth 3`
+  ) |>
+  mutate(
+    pop_change_2021_2025 = (population_2025 - population_2021) / population_2021 * 100,
+    pop_change_2025_2050 = (population_2050 - population_2025) / population_2025 * 100
+  )
+
+st_write(pop_table, "output/pop_table.csv")
+
+
+
+
+
+
+# municipalities <- c(
+#     "Airdrie",
+#     "Medicine Hat",
+#     "Drumheller",
+#     "Chestermere",
+#     "Calmar")
+
+# msn_subset <- msn_alberta |>
+#   filter(R_PLACE %in% municipalities)
+
+# leaflet(msn_subset |> st_transform(crs = 4326)) |>
+#   addTiles() |>
+#   addPolylines(
+#     weight = 3,
+#     opacity = 0.8,
+#     label = ~R_STNAM,
+#     popup = ~paste0(
+#       "<b>", R_STNAM, "</b><br>",
+#       "<em>", R_PLACE, "</em>", "<br>"
+#     )
+#   )
